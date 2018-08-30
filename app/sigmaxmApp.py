dc7fd3551211b2efe2b6ee158d52762ed12f9de0
saikiran rallabandi <s_rallabandi@apple.com>
2018-08-29 23:38:32 -0700

    Changed the structure

--- app/sigmaxmApp.py
+++ app/sigmaxmApp.py
@@ -38,33 +38,33 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 _awsclient=type('AWSClient', (object,), {})()
 
-# Initialize Celery
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+# # Initialize Celery
+# celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+# celery.conf.update(app.config)
 
 
 
 USERNAME = {}
 
-@celery.task(bind=True)
-def long_task(self):
-    """Background task that runs a long function with progress reports."""
-    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
-    message = ''
-    total = random.randint(10, 50)
-    for i in range(total):
-        if not message or random.random() < 0.25:
-            message = '{0} {1} {2}...'.format(random.choice(verb),
-                                              random.choice(adjective),
-                                              random.choice(noun))
-        self.update_state(state='PROGRESS',
-                          meta={'current': i, 'total': total,
-                                'status': message})
-        time.sleep(1)
-    return {'current': 100, 'total': 100, 'status': 'Task completed!',
-            'result': 42}
+# @celery.task(bind=True)
+# def long_task(self):
+#     """Background task that runs a long function with progress reports."""
+#     verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+#     adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+#     noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+#     message = ''
+#     total = random.randint(10, 50)
+#     for i in range(total):
+#         if not message or random.random() < 0.25:
+#             message = '{0} {1} {2}...'.format(random.choice(verb),
+#                                               random.choice(adjective),
+#                                               random.choice(noun))
+#         self.update_state(state='PROGRESS',
+#                           meta={'current': i, 'total': total,
+#                                 'status': message})
+#         time.sleep(1)
+#     return {'current': 100, 'total': 100, 'status': 'Task completed!',
+#             'result': 42}
 
 
 # @app.route('/404')
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from sigmaxmApp import app
+from app.sigmaxmApp import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=True)
